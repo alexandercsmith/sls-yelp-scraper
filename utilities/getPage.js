e4206b8d4eef44c7c7616e3a1a52ff961f385f5c
Alexander Smith <acsmith.dev@gmail.com>
2019-03-16 14:41:48 -0400

    parse data from yelp page

--- utilities/getPage.js
+++ utilities/getPage.js
@@ -1,7 +1,6 @@
 const request = require('request-promise');
 
 module.exports = bizName => {
-  // Get Yelp Page
   const url = `https://www.yelp.com/biz/${bizName}`;
   return request({ method: 'GET', url: url });
 }
--- utilities/parsePage.js
+++ utilities/parsePage.js
@@ -1,4 +1,11 @@
+const cheerio = require('cheerio');
+
 module.exports = page => {
-  // Parse Yelp Page
-  
+  try {
+    const $ = cheerio.load(page);
+    const rating = $('.rating-info .i-stars').attr('title');
+    const reviewCount = $('.rating-info .review-count').text();
+  } catch(error) {
+    return Promise.reject(`Error parsing page: ${JSON.stringify(error)}`)
+  }
 }
