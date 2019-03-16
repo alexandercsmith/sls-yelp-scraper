ccccb234675511315d782fb1bd9a1d5949fa9077
Alexander Smith <acsmith.dev@gmail.com>
2019-03-16 13:04:20 -0400

    add get request for yelp page

--- utilities/getPage.js
+++ utilities/getPage.js
@@ -1,4 +1,7 @@
+const request = require('request-promise');
+
 module.exports = bizName => {
   // Get Yelp Page
   const url = `https://www.yelp.com/biz/${bizName}`;
+  return request({ method: 'GET', url: url });
 }
